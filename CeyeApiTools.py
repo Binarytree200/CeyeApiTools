166a14304063f43114f7a3251681a1e8c2ba8afb
二叉树 <33973549+Binarytree200@users.noreply.github.com>
2021-09-04 14:47:19 +0800

    first commit

---
+++ CeyeApiTools.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   ceyeApiTools.py
+@Time    :   2021/09/02 14:09:56
+@Author  :   Binarytree200 
+@Version :   1.0
+@Desc    :   None
+@Github  :   https://github.com/Binarytree200/CeyeApiTools
+'''
+
+
+import requests
+import json
+import argparse
+
+
+
+
+
+def cecy_id(url1):
+    r = requests.get(url=url1)
+    datadict = json.loads(r.text)
+    str1 = datadict['data'][0]
+    str3= str1['id']
+    return str3,str1
+
+def run_dns(str3):
+    str1=str3
+    id = str1['id']
+    name = str1['name']
+    remote_addr = str1['remote_addr']
+    created_at = str1['created_at']
+    return id,name,remote_addr,created_at
+
+def run_http(str3):
+    str1=str3
+    id = str1['id']
+    name = str1['name']
+    remote_addr = str1['remote_addr']
+    method = str1['method']
+    user_agent = str1['user_agent']
+    data = str1['data']
+    content_type = str1['content_type']
+    created_at = str1['created_at']
+    return id,name,remote_addr,method,user_agent,data,content_type,created_at
+
+def main():
+    parser = argparse.ArgumentParser(description="ceyeApi")
+    parser.add_argument("--token","-t",type=str,required=True,help='请输入token')
+    parser.add_argument("--mold","-m",type=str,default='dns',help='请输入要是查询的类型 DNS or HTTP')
+    parser.add_argument("--filter","-f",type=str,default='ceye.io',help='url匹配规则')
+    args = parser.parse_args()
+    url="http://api.ceye.io/v1/records?token=%s&type=%s&filter=%s"%(args.token,args.mold,args.filter)
+    data3,str123=cecy_id(url)
+
+    while True:
+        data,str1=cecy_id(url)
+        if data3!=data:
+            data3=data
+            if args.mold=='dns':
+                id,name,remote_addr,created_at = run_dns(str1)
+                print ("ID:%s\n触发地址:%s\n触发IP:%s\n触发时间:%s"%(id,name,remote_addr,created_at))
+                print ("\n######################################################################################\n")
+            else:
+                id,name,remote_addr,method,user_agent,data,content_type,created_at = run_http(str1)
+                print ("ID:%s\n触发地址:%s\n触发IP:%s\n触发方式:%s\nUA:%s,Data:%s\nContent_Type:%s\n触发时间:%s"%(id,name,remote_addr,method,user_agent,data,content_type,created_at ))
+                print ("\n######################################################################################\n")
+            fo = open("log.txt", "a+")
+            fo.write(str(str1))
+            fo.write('\n')
+            fo.close()
+
+if __name__=="__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # CeyeApiTools
-CeyeApiTools 一个便捷的CeyeDNSlog返回信息监听工具
+  CeyeApiTools 一个便捷的CeyeDNSlog返回信息监听工具
+
+#### CEYE.IO介绍
+
+  [CEYE.IO](http://ceye.io/)平台，通过自己的 DNS 服务器和 HTTP 服务器监控 DNS 查询和 HTTP 请求。它可以帮助安全研究人员在测试漏洞（例如 SSRF/XXE/RFI/RCE）时收集信息。
+
+  对于每个用户，有一个随机六个字符来与ceye.io组合成的子域名，可以在个人资料页面中找到。记录子域和以下的所有 DNS 查询和 HTTP 请求。例如，`b182oj`是某人的唯一标识符代码，`b182oj.ceye.io`是他/她的子域。对于所有的DNS 请求和HTTP请求`b182oj.ceye.io`和`*.b182oj.ceye.io`将被记录。所有记录都可以从服务器导出，通过处理这些访问日志，研究人员能够确认和改进他们的研究。
+
+#### CeyeApiTools介绍
+
+  CeyeApiTools是根据[CEYE.IO](http://ceye.io/)平台开放出的API接口演化出的，一款便捷查询返回信息监听记录的小工具。（PS：主要原因是我比较懒所以才写的）
+
+##### 使用方法
+
+```
+python CeyeApiTools.py -t ”你的ceye api token” -m ”你要查询的类型 DNS or HTTP” -f ”url匹配规则”
+```
+
+例如：
+
+![image](https://github.com/)
+
+
+
+同时原始数据会保存在执行CeyeApiTools.py文件的目录下log.txt文件内
